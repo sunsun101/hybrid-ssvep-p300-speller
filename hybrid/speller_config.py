b1447de8b2d0d3d3d54a2726c9b4c6d65780b7f2
Sunsun <34477116+sunsun101@users.noreply.github.com>
2023-05-19 20:55:36 +0700

    discrepancy in the number of events

--- hybrid/speller_config.py
+++ hybrid/speller_config.py
@@ -12,10 +12,10 @@ DISPLAY_BOX_SIZE = [1700,100]
 DISPLAY_BOX_POS = (0,450)
 
 SIZE: int = 100
-NUM_BLOCK: int = 8
-NUM_TRIAL: int = 2
+NUM_BLOCK: int = 15
+NUM_TRIAL: int = 1
 NUM_SESSION: int = 3
-EPOCH_DURATION: float = 1
+EPOCH_DURATION: float = 3
 ITI_DURATION: float = 0.1
 CUE_DURATION: float = 1
 NO_SUBSPELLER: int = 4
@@ -46,7 +46,7 @@ SUBSPELLERS:dict = {"1": ["A", "B", "E", "F"],
 
 SERIAL_PORT:str = "COM3"
 BOARD_ID:int = 8
-PARTICIPANT_ID:str = "manish_4target_20230517_v2"
+PARTICIPANT_ID:str = "sunsun_4target_20230519_3sec"
 RECORDING_DIR:str = "hybrid/record"
 TYPE_OF_FILE:str = ".fif"
 CSV_DIR:str = "csv/"
